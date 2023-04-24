176e9f4bf8125528e10d631081c2a5df7c0e46a4
Jin-Yanhong <820877998@qq.com>
2023-04-24 08:43:46 +0800

    update cesium map

--- src/components/CesiumMap/index.vue
+++ src/components/CesiumMap/index.vue
@@ -15,16 +15,6 @@ onMounted(() => {
 	const viewer = new Cesium.Viewer(cesiumContainer.value as HTMLDivElement, {
 		terrainProvider: Cesium.createWorldTerrain(),
 	});
-
-	const buildingTileset = viewer.scene.primitives.add(Cesium.createOsmBuildings());
-
-	viewer.camera.flyTo({
-		destination: Cesium.Cartesian3.fromDegrees(-122.4175, 37.655, 400),
-		orientation: {
-			heading: Cesium.Math.toRadians(0.0),
-			pitch: Cesium.Math.toRadians(-15.0),
-		},
-	});
 });
 </script>
 

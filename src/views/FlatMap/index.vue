c6a9ca98dd2625e7bdefe8df507f708ec520eba9
Jin-Yanhong <820877998@qq.com>
2023-05-05 23:35:40 +0800

    feat scroll notifycation

--- src/views/FlatMap/index.vue
+++ src/views/FlatMap/index.vue
@@ -1,56 +1,56 @@
 <template>
 	<div class="view">
 		<div class="container">
-			<MapOl @onMapReady="onMapReady">
+			<!-- <MapOl @onMapReady="onMapReady">
 				<div ref="overlayContainer" class="overlay">Hello, I'm here</div>
-			</MapOl>
+			</MapOl> -->
 		</div>
 
 		<div class="flex-between topBtnGroup">
 			<div class="left">
-				<el-button size="small" type="primary">治安数据</el-button>
-				<el-button size="small" type="primary">消防数据</el-button>
-				<el-button size="small" type="primary">交通数据</el-button>
-				<el-button size="small" type="primary">气象数据</el-button>
-				<el-button size="small" type="primary">其他数据</el-button>
+				<ElButton size="small" type="primary">治安数据</ElButton>
+				<ElButton size="small" type="primary">消防数据</ElButton>
+				<ElButton size="small" type="primary">交通数据</ElButton>
+				<ElButton size="small" type="primary">气象数据</ElButton>
+				<ElButton size="small" type="primary">其他数据</ElButton>
 			</div>
 
-			<div class="flex-center center">
-				<div class="notice">
-					<el-tag v-for="item in items" :key="item.label" :type="item.type" class="mx-1" effect="dark">
+			<div class="center" ref="noticeContainer">
+				<div class="notice" ref="notice">
+					<ElTag v-for="item in items" :key="item.label" :type="item.type" class="mx-1" effect="dark">
 						{{ item.label }}
-					</el-tag>
+					</ElTag>
 				</div>
 			</div>
 			<div class="flex-end right">
-				<el-button type="" size="small" round> 当前任务 </el-button>
-				<el-button type="primary" size="small" round> 共计 1024 </el-button>
-				<el-button type="success" size="small" round> 已处理 329 </el-button>
-				<el-button type="info" size="small" round> 需协助 103 </el-button>
-				<el-button type="danger" size="small" round> 需上报 15 </el-button>
+				<ElButton type="" size="small" round> 当前任务 </ElButton>
+				<ElButton type="primary" size="small" round> 共计 1024 </ElButton>
+				<ElButton type="success" size="small" round> 已处理 329 </ElButton>
+				<ElButton type="info" size="small" round> 需协助 103 </ElButton>
+				<ElButton type="danger" size="small" round> 需上报 15 </ElButton>
 			</div>
 		</div>
 
 		<div class="flex-between bottomInfoGroup">
 			<div class="left">
-				<el-button size="small" type="success">当前连接数：3289</el-button>
-				<el-button size="small" type="success">最大连接数：120472</el-button>
-				<el-button size="small" type="success">连接状态良好</el-button>
+				<ElButton size="small" type="success">当前连接数：3289</ElButton>
+				<ElButton size="small" type="success">最大连接数：120472</ElButton>
+				<ElButton size="small" type="success">连接状态良好</ElButton>
 			</div>
 			<div class="center">
-				<el-button size="small" type="success">呼叫其他成员</el-button>
-				<el-button size="small" type="warning">一键呼叫指挥中心</el-button>
+				<ElButton size="small" type="success">呼叫其他成员</ElButton>
+				<ElButton size="small" type="warning">一键呼叫指挥中心</ElButton>
 			</div>
 			<div class="right">
-				<el-button size="small" type="success">网络状态：良好</el-button>
-				<el-button size="small" type="success">通讯是否就绪：已就绪</el-button>
+				<ElButton size="small" type="success">网络状态：良好</ElButton>
+				<ElButton size="small" type="success">通讯是否就绪：已就绪</ElButton>
 			</div>
 		</div>
 	</div>
 </template>
 
 <script lang="ts" setup>
-import { ref } from "vue";
+import { ref, onMounted, onBeforeUnmount } from "vue";
 import Map from "ol/Map";
 import type { TagProps } from "element-plus";
 import MapOl from "@/components/Map/index.vue";
@@ -61,12 +61,62 @@ type Item = { type: TagProps["type"]; label: string };
 const mapIns = ref<Map>();
 const overlayContainer = ref<HTMLDivElement>();
 const items = ref<Array<Item>>([
-	{ type: "danger", label: "纬七路 与 经二十六路 交汇处发生车辆交通事故，造成交通拥堵。请立即前往处理！" },
-	{ type: "success", label: "纬七路 与 经二十六路 交汇处发生车辆交通事故，造成交通拥堵。请立即前往处理！" },
-	{ type: "info", label: "纬七路 与 经二十六路 交汇处发生车辆交通事故，造成交通拥堵。请立即前往处理！" },
-	{ type: "danger", label: "纬七路 与 经二十六路 交汇处发生车辆交通事故，造成交通拥堵。请立即前往处理！" },
-	{ type: "warning", label: "纬七路 与 经二十六路 交汇处发生车辆交通事故，造成交通拥堵。请立即前往处理！" },
+	{ type: "danger", label: "纬七路 与 经二十六路 交汇处发生车辆交通事故，造成交通拥堵。请立即前往处理！1" },
+	{ type: "success", label: "纬七路 与 经二十六路 交汇处发生车辆交通事故，造成交通拥堵。请立即前往处理！2" },
+	{ type: "info", label: "纬七路 与 经二十六路 交汇处发生车辆交通事故，造成交通拥堵。请立即前往处理！3" },
+	{ type: "danger", label: "纬七路 与 经二十六路 交汇处发生车辆交通事故，造成交通拥堵。请立即前往处理！4" },
+	{ type: "warning", label: "纬七路 与 经二十六路 交汇处发生车辆交通事故，造成交通拥堵。请立即前往处理！5" },
 ]);
+
+let myInterval = ref();
+const notice = ref<HTMLDivElement>();
+const noticeContainer = ref<HTMLDivElement>();
+
+onMounted(() => {
+	window.setTimeout(() => {
+		setMymyInterval();
+	}, 2000);
+});
+
+function msgTextParser(input: string): string {
+	let result;
+	const reg = /(?<=translateX\()[^\(\)]*(?=px\))/;
+
+	if (reg.test(input)) {
+		const arr = input.match(reg) || [""];
+		result = arr[0];
+	} else {
+		result = "";
+	}
+	return result;
+}
+
+function setMymyInterval() {
+	const scrollWidth = notice.value?.scrollWidth || 0;
+	const containerWidth = noticeContainer.value?.clientWidth || 0;
+	if (scrollWidth > containerWidth) {
+		let offset = 0;
+		myInterval.value = window.setInterval(() => {
+			const input = notice.value?.style.transform || "";
+			const noticeScroll = Number(msgTextParser(input));
+			if (Math.abs(noticeScroll) <= scrollWidth - containerWidth) {
+				offset += 1;
+				notice.value?.setAttribute("style", `transform: translateX(-${offset}px);`);
+			} else {
+				window.setTimeout(() => {
+					offset = 0;
+					notice.value?.setAttribute("style", `transform: translateX(0px); transition: 1s;`);
+				}, 1200);
+			}
+		}, 10);
+	}
+}
+function clearMymyInterval() {
+	window.clearInterval(myInterval.value);
+}
+onBeforeUnmount(() => {
+	clearMymyInterval();
+});
 
 const onMapReady = (Ins: Map) => {
 	mapIns.value = Ins;
@@ -139,20 +189,20 @@ export default {
 
 		.center {
 			height: 100%;
-			overflow-y: hidden;
+			width: 100%;
+			overflow-x: auto;
+			cursor: pointer;
+			white-space: nowrap;
 
 			.notice {
-				display: flex;
-				cursor: pointer;
-				width: auto;
 				height: 100%;
-				align-items: center;
-				flex-wrap: nowrap;
+				display: table;
+				overflow-x: auto;
+				cursor: pointer;
+				white-space: nowrap;
 
-				& > span {
-					line-height: 24px;
-					display: inline-block;
-					text-align: center;
+				& > .el-tag {
+					transform: translateY(10px);
 				}
 			}
 			&::-webkit-scrollbar {
